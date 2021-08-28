bd3dd6c66b5b7bb82855678f503f1bae558d0381
lemkas <lemov1980@yandex.ru>
2021-08-28 13:08:22 +0300

    added navbar

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const path = require("path");
 const exphbs = require("express-handlebars");
 const app = express();
 
@@ -15,14 +14,28 @@ app.set("view engine", "hbs");
 // указываем где будут находиться шаблоны
 app.set("views", "views");
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", {
+    title: "Главная страница",
+    isHome: true,
+  });
 });
 
 app.get("/courses", (req, res) => {
-  res.render("courses");
+  res.render("courses", {
+    title: "Курсы",
+    isCourses: true,
+  });
 });
 
+app.get("/add", (req, res) => {
+  res.render("courses", {
+    title: "Добавить курс",
+    isAdd: true,
+  });
+});
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
