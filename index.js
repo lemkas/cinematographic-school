19b2be544446f9ff0ac2c6507364c7f32c41f85d
lemkas <lemov1980@yandex.ru>
2021-08-28 12:18:46 +0300

    set up hbs and layout

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 const express = require("express");
+const path = require("path");
+const exphbs = require("express-handlebars");
 const app = express();
+
+//настройка хбрс
+const hbs = exphbs.create({
+  defaultLayout: "main",
+  extname: "hbs",
+});
+
+// регистриуем движок и начинаем использовать
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+// указываем где будут находиться шаблоны
+app.set("views", "views");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/courses", (req, res) => {
+  res.render("courses");
+});
 
 const PORT = process.env.PORT || 3000;
 
